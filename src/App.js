bbe4a0e0710bb4443bd1644caa07e9da36dae036
jiyoonZ <juneky416@gmail.com>
2022-08-04 23:55:24 +0900

    Feat: movie 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -27,16 +28,13 @@ function App() {
           <h1>🎬 Movie App</h1>
           <div>
             {movies.map((movie) => (
-              <div key={movie.id}>
-                <img src={movie.medium_cover_image} alt={movie.title} />
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((g) => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </ul>
-              </div>
+              <Movie
+                key={movie.id}
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
             ))}
           </div>
         </>
